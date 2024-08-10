38e326bb2fe1bd030231d2c988dc8c30d8c74891
ostdari <115879627+ostdari@users.noreply.github.com>
2024-08-10 07:33:31 +0300

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { HashRouter  as Router, Route, Routes, Navigate } from 'react-router-dom';
 import LoginPage from './LoginPage';
 import MyCalendar from './components/MyCalendar';
 import TimeFinder from './components/TimeFinder'; // Импорт компонента поиска времени
